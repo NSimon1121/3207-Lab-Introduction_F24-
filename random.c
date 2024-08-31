f473344c858c2e913e5ded0aff261f877a331111
Nicholas <tup76485@temple.edu>
2024-08-30 20:25:56 -0400

    randomChar function and main

--- random.c
+++ random.c
@@ -1,3 +1,12 @@
-char randomChar(){
-    char randomLetter = 'A' + rand() % 26;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+randomChar()
+{
+        char randomLetter[] = {'A','B','C','D','E','F','G','H','I','J',
+                        'K','L','M','N','O','P','Q','R','S','T','U',
+                        'V','W','X','Y','Z'};
+    int r = rand() % 26;
+    return randomLetter[r];
 }
